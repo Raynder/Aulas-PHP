08986a18b6fd65c7c188e0744c98aceaa758359e
Raynder Cardoso <raynder4@gmail.com>
2021-01-04 20:51:52 -0300

    Implementadno funções na class Usuario

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -1,12 +1,14 @@
 <?php 
 
 class Usuario {
+    //Atributos da class Usuario
 
     private $idusuario;
     private $deslogin;
     private $dessenha;
     private $dtcadastro;
 
+    //Gets and Seters for class Usuario
 
     public function getIdusuario(){
         return $this->idusuario;
@@ -40,6 +42,7 @@ class Usuario {
         $this->dtcadastro = $dt;
     }
 
+    //Função que puxa todos os valores por um ID
     public function loadById($id){
         $sql = new SQL;
 
@@ -55,6 +58,22 @@ class Usuario {
 
         }
     }
+    //Função retorna todos os valores do banco
+
+    public static function getList(){
+        $sql = new Sql;
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+    //FUNÇÃO QUE BUSCA POR REFERENCIA DE UMA STRING
+
+     static function search($login){
+        $sql = new Sql;
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH' => "%".$login."%"));
+    }
+
+    //Funcion que sera ativa semque que for feito um echo no objeto
 
     public function __toString(){
         return json_encode(array(
@@ -64,6 +83,25 @@ class Usuario {
             "dtcadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
         ));
     }
+
     
+    public function login($login, $pass){
+        $sql = new SQL;
+
+        $resultado = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :DESLOGIN AND dessenha = :DESSENHA", array(":DESLOGIN"=>$login,":DESSENHA"=>$pass));
+
+        if(count($resultado) > 0){
+            $row = $resultado[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        }
+        else{
+            throw new Exception("Login e/ou Senha invalidos.");
+        }
+    }
 
 }
--- DAO/index.php
+++ DAO/index.php
@@ -2,7 +2,22 @@
 
     require_once "config.php";
 
+
+    /* FUNÇÃO QUE TRAS APENAS UM USUARIO
     $user = new Usuario;
     $user->loadById(3);
+    */
 
+    /*FUNÇÃO QUE TRAS TODOS OS USUARIOS
+    $lista = Usuario::getList();
+    echo json_encode($lista);
+    */
+
+    /*FUNÇÃO QUE BUSCA LISTA DE USUARIOS POR PELO LOGIN
+    $busca = Usuario::search("jo");
+    echo json_encode($busca);
+    */
+
+    $user = new Usuario;
+    $user->login("joe", "12345678");
     echo $user;
